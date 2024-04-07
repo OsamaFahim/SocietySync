d96b2e77179512199c31a78865c347cb1fe4792a
SyedMuhaimen01 <i210888@nu.edu.pk>
2024-04-07 22:09:06 +0500

    AdminProfile

---
+++ Pages/AdminProfile.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace SocietySync.Pages
+{
+    public class AdminControllerModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
