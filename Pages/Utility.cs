c25c09cb07952dfa495d115d3b475fe3ac60eb1d
SyedMuhaimen01 <i210888@nu.edu.pk>
2024-05-18 00:21:45 +0500

    final

--- Pages/Utility.cs
+++ Pages/Utility.cs
@@ -8,7 +8,7 @@ namespace SocietySync.Pages
     {
         public string AddSocietyData_to_ViewModel(Society SelectedSociety)
         {
-            var Selected_Society_Data = $"Soceity Name: {SelectedSociety.Name}\n\n";
+            var Selected_Society_Data = $"Society Name: {SelectedSociety.Name}\n\n";
             Selected_Society_Data += $"Requester Roll-Num: {SelectedSociety.PresidentRollNum}\n\n";
             Selected_Society_Data += $"Category: {SelectedSociety.Category}\n\n";
             Selected_Society_Data += $"Goals/Objectives: {SelectedSociety.Goals}\n\n";
